ecad2d56d9ef3c31a589b3398816fde318c659d2
Rekha <rekha.bothiraj@bglgroup.co.uk>
2013-12-04 15:01:40 +0000

    name change for classes

---
+++ Src/FactoryPattern/DecoratorPattern.cs
@@ -0,0 +1,7 @@
+﻿namespace FactoryPattern
+{
+    public class DecoratorPattern
+    {
+         
+    }
+}
---
+++ Src/Tests/FactoryPatternTests.cs
@@ -0,0 +1,21 @@
+﻿using FactoryPattern;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class FactoryPatternTests
+    {
+        [TestCase("car", "car")]
+        [TestCase("Bike", "bike")]
+        [TestCase("carkjfkldjfksdjfk", "undefined")]
+        public void ShouldCreateaBusinessClassForTheSelectedProduct(string product, string expected)
+        {
+            //call the factory class
+            var factoryclass = new Factoryclass();
+            Vehicle businessObject = factoryclass.GetTheSelecteditem(product.ToLower());
+            Assert.IsInstanceOf(typeof (Vehicle), businessObject);
+            Assert.That(businessObject.Buy(), Is.EqualTo(expected));
+        }
+    }
+}
